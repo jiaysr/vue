43942a9c8b5ea6e20a20f30073a16bf275f7f3e2
jiaysr <1792388950@qq.com>
2023-11-08 11:14:32 +0800

    1.1

--- src/components/prop/App.vue
+++ src/components/prop/App.vue
@@ -5,26 +5,18 @@
     data(){
       return{
         ad:0,
-        logo:false
+        logo:false,
+        logo1:true
       }
-    },methods:{
-      add(){
-        if(this.ad==0){
-          this.logo=true;
-          this.ad++
-        }else if(this.ad==1){
-          this.logo=false
-          this.ad=0
-        }
-      }
-    }
+    },
   }
 </script>
 
 <template>
   <div><h1>这是app组件11</h1></div>
-  <div class="back" :class="logo ? 'ba':''"></div>
-  <button @click="add()">点我添加颜色</button>
+  <div class="back" :class="[logo ? 'ba':'',logo1 ? 'da':'']"></div>
+  <button @click="logo=!logo">点我添加颜色</button>
+  <button @click="logo1=!logo1">点我添加颜色</button>
   <myprops title="队伍的编号" authou="吴滴滴"></myprops>
 </template>
 
@@ -36,5 +28,8 @@
   }
   .ba{
     background-color: #0f6674;
+  }
+  .da{
+    background-color: red;
   }
 </style>
